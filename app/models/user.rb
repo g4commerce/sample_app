8469d390505eb95200d535d62d9d6c641293221a
David Gallello <david@gallello.com>
2012-12-27 22:56:15 -0700

    Refactor User model code

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-	before_save { |user| user.email = email.downcase }
+	before_save { |user| user.email = email.downcase } # Could also be self.email.downcase!
 
 	validates(:name, presence: true, length: { maximum: 50 })
 	validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false })
