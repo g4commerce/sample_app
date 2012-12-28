0a38505593519a4a51bb447bd9fbde61d2097b8f
David Gallello <david@gallello.com>
2012-12-28 01:14:40 -0700

    Fix gravatar image problem

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
 	# Returns Gravatar for the given user (http://www.gravatar.com)
 	def gravatar_for(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url = "http://0.gravatar.com/avatars/#{gravatar_id}"
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?r=g"
 		image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end
 end
